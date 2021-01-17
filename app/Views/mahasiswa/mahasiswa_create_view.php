58558b3e659d31c936dc292006a13f34315b77fd
Sigit Prasetyo Noprianto <noprisigit@gmail.com>
2021-01-17 20:42:43 +0700

    create view for add mahasiswa

---
+++ app/Views/mahasiswa/mahasiswa_create_view.php
@@ -0,0 +1,94 @@
+<?= $this->extend('layout/body'); ?>
+<?= $this->section('content'); ?>
+
+<div class="content-wrapper">
+  <section class="content-header">
+    <div class="container-fluid">
+      <div class="row mb-2">
+        <div class="col-sm-6">
+          <h1>Tambah Data Mahasiswa</h1>
+        </div>
+        <div class="col-sm-6">
+          <ol class="breadcrumb float-sm-right">
+            <li class="breadcrumb-item"><a href="/home">Home</a></li>
+            <li class="breadcrumb-item"><a href="<?= base_url('mahasiswa') ?>">Mahasiswa</a></li>
+            <li class="breadcrumb-item active">Create</li>
+          </ol>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <section class="content">
+    <div class="card card-solid">
+      <form role="form">
+        <div class="card-body">
+          <div class="row">
+            <div class="col">
+              <div class="form-group">
+                <label for="exampleInputEmail1">N.I.M</label>
+                <input type="number" class="form-control" name="nim" placeholder="NIM Mahasiswa">
+              </div>
+            </div>
+            <div class="col">
+              <div class="form-group">
+                <label for="exampleInputEmail1">Nama</label>
+                <input type="text" class="form-control" name="nama" placeholder="Nama Mahasiswa">
+              </div>
+            </div>
+          </div>
+          <div class="row">
+            <div class="col">
+              <div class="form-group">
+                <label for="exampleInputEmail1">Tempat Lahir</label>
+                <input type="text" class="form-control" name="tempat_lahir" placeholder="Tempat Lahir">
+              </div>
+            </div>
+            <div class="col">
+              <div class="form-group">
+                <label for="exampleInputEmail1">Tanggal Lahir</label>
+                <input type="date" class="form-control" name="tanggal_lahir" placeholder="Tanggal Lahir">
+              </div>
+            </div>
+          </div>
+          <div class="row">
+            <div class="col">
+              <div class="form-group">
+                <label for="exampleInputEmail1">Email</label>
+                <input type="email" class="form-control" name="email" placeholder="Tanggal Lahir">
+              </div>
+            </div>
+            <div class="col">
+              <div class="form-group">
+                <label for="exampleInputEmail1">Jenis Kelamin</label>
+                <select name="jenis_kelamin" id="jenis_kelamin" class="form-control">
+                  <option selected disabled>-- Pilih Jenis Kelamin --</option>
+                  <option value="Laki-Laki">Laki-Laki</option>
+                  <option value="Perempuan">Perempuan</option>
+                </select>
+              </div>
+            </div>
+          </div>
+          <div class="row">
+            <div class="col">
+              <div class="form-group">
+                <label for="exampleInputEmail1">N.I.K</label>
+                <input type="number" class="form-control" name="nik" placeholder="N.I.K">
+              </div>
+            </div>
+            <div class="col">
+              <div class="form-group">
+                <label for="exampleInputEmail1">Jurusan</label>
+                <select name="jurusan" id="jurusan" class="form-control"></select>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="card-footer">
+          <button type="submit" class="btn btn-primary">Submit</button>
+        </div>
+      </form>
+    </div>
+  </section>
+</div>
+<?= $this->endSection(); ?>
